71ae258561bd66ca9e629c19159ba9adc89368e2
Edgars Pavlovskis <edgars.pavlovskis@gmail.com>
2024-06-25 12:14:11 +0300

    Update header-black.blade.php

--- resources/views/layouts/header-black.blade.php
+++ resources/views/layouts/header-black.blade.php
@@ -4,9 +4,9 @@
         <div class="row align-items-center">
             <div class="col-lg-6 col-sm-6 col-12">
                 <div class="header-top-dropdown text-white">
-                    <i class="fa-solid fa-phone me-2"></i> <a class="text-white" href="tel:+37126511569">+371&nbsp;<b>26511569</a></b>
+                    <i class="fa-solid fa-phone me-2"></i> <a class="text-white" href="tel:+37128372929">+371&nbsp;<b>28372929</a></b>
                     &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-                    <i class="fa-solid fa-envelope me-2"></i> <a class="text-white" href="mailto:info@birojamunskolai.lv">info@birojamunskolai.lv</a>
+                    <i class="fa-solid fa-envelope me-2"></i> <a class="text-white" href="mailto:pakalpojumi@alba-ltd.lv">pakalpojumi@alba-ltd.lv</a>
 
                     <div class="social-share ms-3">
 
