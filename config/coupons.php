93b6ffbde3dbece0ac794d96174d889bf8fd4364
Edgars Pavlovskis <edgars.pavlovskis@gmail.com>
2024-08-28 13:12:28 +0300

    Update coupons.php

--- config/coupons.php
+++ config/coupons.php
@@ -12,6 +12,16 @@ return [
             'datestart' => '2024-08-01', // YYYY-MM-DD format
             'dateend' => '2024-09-30',   // YYYY-MM-DD format
         ],
+        'PIRKUMS20' => [
+            'title' => ['lv'=>'-20% atlaide iepērkoties birojamunskolai.lv'],
+            'badge-class' => 'text-bg-warning',
+            'discount'=> [
+                'amount' => 20,
+                'ignored'=> [],
+            ],
+            'datestart' => '2024-08-28', // YYYY-MM-DD format
+            'dateend' => '2024-10-15',   // YYYY-MM-DD format
+        ],
     ]
 ];
 
