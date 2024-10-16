a69e087e3541516a55729d27d019db2b22400c22
Edgars Pavlovskis <edgars.pavlovskis@gmail.com>
2024-10-16 09:11:18 +0300

    Create 2024_10_16_090844_modify_delivery_price_column_in_orders_table.php

---
+++ database/migrations/2024_10_16_090844_modify_delivery_price_column_in_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            // Change deliveryPrice from smallint to decimal(10, 2)
+            $table->decimal('deliveryPrice', 10, 2)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            // Revert deliveryPrice back to smallint
+            $table->smallInteger('deliveryPrice')->change();
+        });
+    }
+};
